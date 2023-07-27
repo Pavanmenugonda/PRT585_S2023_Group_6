c61af41b987d471bd83fab7b7120412c8e76df0d
Mehedi Hasan Ridoy <53724550+Mridoy7@users.noreply.github.com>
2023-07-28 01:52:00 +0930

    Week_3 WebAPI_CRUD
    WebAPI_CRUD

---
+++ Mridoy-s358194/week 3/wep_API_CRUD/MVC_Enrolment_demo/Data/MVCDemoDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Enrolment_demo.Models.Domain;
+
+namespace MVC_Enrolment_demo.Data
+{
+    public class MVCDemoDBContext : DbContext
+    {
+        public MVCDemoDBContext(DbContextOptions options) : base(options)
+        {
+        }
+          
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
