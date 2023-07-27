710bc2deb88b2774fdef119212e40d1f68e18442
Md Habib Sarker <97721511+MdHabib134@users.noreply.github.com>
2023-07-28 01:07:59 +0930

    StudentsAPI

---
+++ MdHabibSarker_S361101/Week-1/StudentsAPI/StudentsAPI/Data/StudentsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsAPI.Models;
+
+namespace StudentsAPI.Data
+{
+    public class StudentsDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasNoKey();
+        }
+        public StudentsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
