a6688437f2f06297416e05480ce846be262af94e
Mehedi Hasan Ridoy <53724550+Mridoy7@users.noreply.github.com>
2023-07-28 15:30:05 +0930

    update Web-api

---
+++ Mridoy-s358194/week 2/wep_API_CRUD/MVC_Enrolment_demo/Migrations/20230727144658_initial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Enrolment_demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
