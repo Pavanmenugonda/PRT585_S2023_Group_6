dedd905fd8bff681922095fb8a214e071cfa5c40
bthapaji <solobishnu123@gmail.com>
2023-07-30 21:07:48 +0930

    add week 2-3 source

---
+++ BishnuThapa_S361373/Week 2/week 2 angular/myapp/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myapp';
+
+  rows= [
+    {
+      "Id":1,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    },
+    {
+      "Id":2,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    }, 
+    {
+      "Id":3,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    }, 
+    {
+      "Id":4,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    }, 
+    {
+      "Id":5,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    }, 
+    {
+      "Id":6,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    },
+     {
+      "Id":7,
+      "Name":'Bishnu',
+      "Age": 26,
+      "Gender" : 'Male',
+      "Country" :'Nepal'
+    },
+  ]
+
+}
