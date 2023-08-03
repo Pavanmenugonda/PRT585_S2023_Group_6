140537502aaf636a182e4bf1d05c18aee50a55cd
Chris <115781090+CDUChris@users.noreply.github.com>
2023-08-04 03:48:45 +0930

    week3/extension update

--- ChrisXiang_s349548/week_3/movie/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace movie.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
