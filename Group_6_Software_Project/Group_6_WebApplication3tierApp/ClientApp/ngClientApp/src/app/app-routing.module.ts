24a9c6eba3247f1103d2f75f41d37ceb0656d44c
anvong <anvong@gmail.com>
2023-09-03 14:11:10 +0930

    ng client Add student form

--- Group_6_Software_Project/Group_6_WebApplication3tierApp/ClientApp/ngClientApp/src/app/app-routing.module.ts
+++ Group_6_Software_Project/Group_6_WebApplication3tierApp/ClientApp/ngClientApp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonComponent } from './components/person/person.component';
 import { StudentComponent } from './components/student/student.component';
+import { StudentAddEditComponent } from './student-add-edit/student-add-edit.component';
 const routes: Routes = [
   { path: 'person', component: PersonComponent },
   { path: 'student', component: StudentComponent },
+  { path: 'student/add', component: StudentAddEditComponent },
 ];
 
 @NgModule({
--- Group_6_Software_Project/Group_6_WebApplication3tierApp/ClientApp/ngClientApp/src/app/app.module.ts
+++ Group_6_Software_Project/Group_6_WebApplication3tierApp/ClientApp/ngClientApp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,16 +9,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PersonComponent } from './components/person/person.component';
 import { StudentComponent } from './components/student/student.component';
 import { GridModule } from '@progress/kendo-angular-grid';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ButtonsModule } from "@progress/kendo-angular-buttons";
-import { InputsModule } from "@progress/kendo-angular-inputs";
-
+import { InputsModule, TextBoxModule } from "@progress/kendo-angular-inputs";
+import { StudentAddEditComponent } from './student-add-edit/student-add-edit.component';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
-    StudentComponent
+    StudentComponent,
+    StudentAddEditComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +33,12 @@ import { InputsModule } from "@progress/kendo-angular-inputs";
     FormsModule,
     ButtonsModule,
     InputsModule,
+    TextBoxModule,
+    LabelModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    DropDownsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
